fb296e85252c42174d1a2e96d6c3cc2525b4cf22
ViniciusEDcampos <viniciuseduardo1403@gmail.com>
2025-09-19 11:24:25 -0300

    feat(minha-parte): adição da navbar funcional separada no layout

---
+++ src/views/layout/header.php
@@ -0,0 +1,40 @@
+
+<header>
+  <nav id="navigation" class="navbar navbar-expand-lg px-4 fixed-top">
+    <div class="nav-left">
+      <img src="/public/img/LogoIcon.png" id="imgLogo" alt="Logo">
+      <div class="LogoText">
+        <a href="#" class="logo" >Fokus</a>
+        <span>Plataforma de Estudos</span>
+      </div>
+    </div>
+
+    <!--  Menu mobile -->
+    <button class="navbar-toggler menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="bar .text-primary"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </button>
+
+    <div class="collapse navbar-collapse navmenu-container" id="navbarMenu">
+      <ul class="navmenu">
+        <li class="nav-item"><a href="#"><i class="fi fi-rs-home"></i> Inicio</a></li>
+        <li class="nav-item"><a href="#"><i class="fi fi-rr-alarm-clock"></i> Cronometro</a></li>
+        <li class="nav-item"><a href="index.html"><i class="fi fi-rs-edit"></i> Tarefas</a></li>
+        <li class="nav-item"><a href="#"><i class="fi fi-rr-trophy"></i> Ranking</a></li> 
+        <li class="nav-item"><a href="Material.html"><i class="fi fi-rs-document"></i> Material</a></li>
+        <li class="nav-item"><a href="#"><i class="fi fi-rr-chat-arrow-grow"></i> Andamento</a></li>
+      </ul>
+
+      <div class="nav-right mt-3 mt-lg-0">
+        <div class="btn-box">
+          <button id="btn-tema"><i class="fi fi-rr-moon"></i></button>
+        </div>
+        <div class="user">
+          <div class="user-icon">U</div>
+          <span>Usuário<br><small>Estudante</small></span>
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>
--- src/views/pdfViewer.php
+++ src/views/pdfViewer.php
@@ -12,58 +12,19 @@
 <link rel="stylesheet" href="/public/CSS/header/header.css">
 <link rel="stylesheet" href="/public/CSS/style.css">
 
-<link rel="stylesheet" href="CSS/style.css">
-
 <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
 <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-straight/css/uicons-regular-straight.css'>
 <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
 <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-bold-rounded/css/uicons-bold-rounded.css'>
+<link rel="stylesheet" href="/src/views/layout/header.php">
 </head>
 <body>
 
        <div class="background"></div>
 
-<header>
-  <nav id="navigation" class="navbar navbar-expand-lg px-4 fixed-top">
-    <div class="nav-left">
-      <img src="/public/img/LogoIcon.png" id="imgLogo" alt="Logo">
-      <div class="LogoText">
-        <a href="#" class="logo" >Fokus</a>
-        <span>Plataforma de Estudos</span>
-      </div>
-    </div>
-
-    <!--  Menu mobile -->
-    <button class="navbar-toggler menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="bar .text-primary"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
-    </button>
-
-    <div class="collapse navbar-collapse navmenu-container" id="navbarMenu">
-      <ul class="navmenu">
-        <li class="nav-item"><a href="#"><i class="fi fi-rs-home"></i> Inicio</a></li>
-        <li class="nav-item"><a href="#"><i class="fi fi-rr-alarm-clock"></i> Cronometro</a></li>
-        <li class="nav-item"><a href="index.html"><i class="fi fi-rs-edit"></i> Tarefas</a></li>
-        <li class="nav-item"><a href="#"><i class="fi fi-rr-trophy"></i> Ranking</a></li> 
-        <li class="nav-item"><a href="Material.html"><i class="fi fi-rs-document"></i> Material</a></li>
-        <li class="nav-item"><a href="#"><i class="fi fi-rr-chat-arrow-grow"></i> Andamento</a></li>
-      </ul>
-
-      <div class="nav-right mt-3 mt-lg-0">
-        <div class="btn-box">
-          <button id="btn-tema"><i class="fi fi-rr-moon"></i></button>
-        </div>
-        <div class="user">
-          <div class="user-icon">U</div>
-          <span>Usuário<br><small>Estudante</small></span>
-        </div>
-      </div>
-    </div>
-  </nav>
-</header>
+       <?php include __DIR__ . "/layout/header.php"; ?>
 
  <main class="container-principal">
     <section id="home" class="container">
